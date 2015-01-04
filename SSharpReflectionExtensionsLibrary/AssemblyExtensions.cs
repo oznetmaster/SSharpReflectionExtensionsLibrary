9e758ebce1a997622d8e48e8dedfb2ee432e4c42
oznetmaster <ncolvin@marvelous.com>
2015-01-04 10:32:32 +0800

    Initial Commit

---
+++ SSharpReflectionExtensionsLibrary/AssemblyExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System;
+using Crestron.SimplSharp;
+using Crestron.SimplSharp.CrestronIO;
+using Crestron.SimplSharp.CrestronXmlLinq;
+using Crestron.SimplSharp.CrestronXml;
+using System.Text;
+
+namespace Crestron.SimplSharp.Reflection
+	{
+	public class AssemblyEx
+		{
+		public static Assembly GetEntryAssembly ()
+			{
+			string appDir = InitialParametersClass.ProgramDirectory.ToString ();
+
+			if (CrestronEnvironment.RuntimeEnvironment == eRuntimeEnvironment.SIMPL)
+				return null;
+
+			var proginfo = File.ReadToEnd (Path.Combine (appDir, "ProgramInfo.config"), Encoding.UTF8);
+			var doc = XDocument.Parse ("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\r\n" + proginfo);
+			var entry = doc.Descendants ("EntryPoint").FirstOrDefault ();
+			if (entry == null)
+				return null;
+
+			return Assembly.Load (entry.Value);
+			}
+		}
+	}
---
+++ SSharpReflectionExtensionsLibrary/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle ("SSharpReflectionExtensionsLibrary")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct ("SSharpReflectionExtensionsLibrary")]
+[assembly: AssemblyCopyright ("Copyright ©  2014")]
+[assembly: AssemblyVersion ("1.0.0.*")]
+
