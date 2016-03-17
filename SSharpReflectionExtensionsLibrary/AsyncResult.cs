51e86a5b73d9c58c6acda14593621766d89d7b34
oznetmaster <ncolvin@marvelous.com>
2016-03-17 08:28:48 +0800

    Add AsyncResult and Delegate Extensions
    Add Delegate.BeginInvokeEx and Delegate.EndInvokeEx to support
    asynchronouse deleagte invocation

---
+++ SSharpReflectionExtensionsLibrary/AsyncResult.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Crestron.SimplSharp;
+
+namespace Crestron.SimplSharp.CrestronIO
+	{
+	public class AsyncResult : IAsyncResult
+		{
+		private readonly CEvent cevent = new CEvent (false, false);
+
+		#region IAsyncResult Members
+
+		public object AsyncState
+			{
+			get;
+			internal set;
+			}
+
+		public bool CompletedSynchronously
+			{
+			get;
+			internal set;
+			}
+
+		public bool IsCompleted
+			{
+			get;
+			internal set;
+			}
+
+		public CEventHandle AsyncWaitHandle
+			{
+			get { return cevent; }
+			}
+
+		public object InnerObject
+			{
+			get { throw new NotImplementedException (); }
+			}
+
+		#endregion
+
+		public bool EndInvokeCalled
+			{
+			get;
+			internal set;
+			}
+
+		public Delegate AsyncDelegate
+			{
+			get;
+			internal set;
+			}
+
+		internal object Result
+			{
+			get;
+			set;
+			}
+
+		internal AsyncResult (Delegate asyncDelegate, object obj)
+			{
+			AsyncDelegate = asyncDelegate;
+			AsyncState = obj;
+			}
+		}
+	}
--- SSharpReflectionExtensionsLibrary/DelegateExtensions.cs
+++ SSharpReflectionExtensionsLibrary/DelegateExtensions.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Crestron.SimplSharp;
+using Crestron.SimplSharp.CrestronIO;
+using Crestron.SimplSharp.Reflection;
+using IAsyncResult = Crestron.SimplSharp.CrestronIO.IAsyncResult;
+using AsyncCallback = Crestron.SimplSharp.CrestronIO.AsyncCallback;
 
-namespace Crestron.SimplSharp.Reflection
+namespace Crestron.SimplSharp
 	{
 	public static class DelegateExtensions
 		{
@@ -25,6 +29,74 @@ namespace Crestron.SimplSharp.Reflection
 		public static object DynamicInvoke (this Delegate dlg, params object[] args)
 			{
 			return dlg.GetMethod().Invoke (dlg.Target, BindingFlags.Default, null, args, null);
+			}
+
+		/// <summary>
+		/// Dynamically invokes (late-bound) the method represented by the current
+		/// delegate. 
+		/// </summary>
+		/// <param name="dlg">Extended class.</param>
+		/// <param name="args">An array of objects that are the arguments to pass to the
+		/// method represented by the current delegate.</param>
+		/// <returns>The object returned by the method represented by the delegate.
+		/// </returns>
+		/// <exception cref="MemberAccessException"> The caller does not have access to the
+		/// method represented by the delegate (for example, if the method is private); The
+		/// number, order, or type of parameters listed in <paramref name="args"/> is invalid. </exception>
+		/// <exception cref="TargetInvocationException">One of the encapsulated methods
+		/// throws an exception.</exception>
+		public static TResult DynamicInvoke<TResult> (this Delegate dlg, params object[] args)
+			{
+			return (TResult)dlg.GetMethod().Invoke (dlg.Target, BindingFlags.Default, null, args, null);
+			}
+
+		private class InvokeInfo
+			{
+			public AsyncResult result;
+			public AsyncCallback callback;
+			public object state;
+			public object[] args;
+			}
+
+		public static IAsyncResult BeginInvokeEx (this Delegate dlg, AsyncCallback callback, object obj, params object[] args)
+			{
+			var iar = new AsyncResult (dlg, obj);
+			var invokeInfo = new InvokeInfo {result = iar, callback = callback, state = obj, args = args};
+			CrestronInvoke.BeginInvoke (DoDelegate, invokeInfo);
+			return iar;
+			}
+
+		private static void DoDelegate (object state)
+			{
+			var invokeInfo = (InvokeInfo)state;
+
+			invokeInfo.result.Result = invokeInfo.result.AsyncDelegate.DynamicInvoke (invokeInfo.args);
+
+			invokeInfo.result.IsCompleted = true;
+			((CEvent)invokeInfo.result.AsyncWaitHandle).Set ();
+			if (invokeInfo.callback != null)
+				invokeInfo.callback (invokeInfo.result);
+			}
+
+		public static object EndInvokeEx (this Delegate dlg, IAsyncResult result)
+			{
+			var asyncResult = result as AsyncResult;
+			if (asyncResult == null)
+				throw new ArgumentException ("invalid IAsyncResult", "result");
+
+			if (asyncResult.EndInvokeCalled)
+				throw new InvalidOperationException ("EndInvoke can only be called onece");
+			asyncResult.EndInvokeCalled = true;
+
+			if (!asyncResult.CompletedSynchronously)
+				asyncResult.AsyncWaitHandle.Wait ();
+
+			return asyncResult.Result;
+			}
+
+		public static TResult EndInvokeEx<TResult> (this Delegate dlg, IAsyncResult result)
+			{
+			return (TResult)dlg.EndInvokeEx (result);
 			}
 		}
 	}
