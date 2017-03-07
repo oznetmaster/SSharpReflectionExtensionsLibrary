3b7767fb15e2cf7524f49160a737cf233cfd497d
oznetmaster <ncolvin@marvelous.com>
2017-03-07 11:36:24 +0800

    Add the MethodBaseEx class
    Implements MethodBase.GetCurrentMethod
    Adds GetCallingMethod, GetCurrentMethodName and GetCallingMethodName

--- SSharpReflectionExtensionsLibrary/AssemblyExtensions.cs
+++ SSharpReflectionExtensionsLibrary/AssemblyExtensions.cs
@@ -9,16 +9,8 @@ using System.Collections.Generic;
 
 namespace Crestron.SimplSharp.Reflection
 	{
-	public class AssemblyEx
+	public static class AssemblyEx
 		{
-		private static readonly List<Assembly> assemblies;
-		static AssemblyEx ()
-			{
-			assemblies = new List<Assembly> ();
-
-			GetAllAssemblies ();
-			}
-
 		public static Assembly GetEntryAssembly ()
 			{
 			string appDir = InitialParametersClass.ProgramDirectory.ToString ();
@@ -35,76 +27,11 @@ namespace Crestron.SimplSharp.Reflection
 			return Assembly.Load (entry.Value);
 			}
 
-		private static string GetCaller ()
-			{
-			string stackTrace;
-			try
-				{
-				throw new Exception ();
-				}
-			catch (Exception ex)
-				{
-				stackTrace = ex.StackTrace;
-				}
-			string[] stackFrames = stackTrace.Split ('\n');
-			if (stackFrames.Length < 4)
-				return null;
-
-			string callingFrame = stackFrames[3].Trim ().Substring (3);
-			return callingFrame;
-			}
-
 		public static Assembly GetCallingAssembly ()
 			{
-			string caller = GetCaller ();
-			string methodName = caller.Substring (0, caller.IndexOf ('('));
-			string callingTypeName = methodName.Substring (0, methodName.LastIndexOf ('.'));
+			var callingType = ReflectionUtilities.GetCallingType (3);
 
-			CType callingType;
-			while (true)
-				{
-				if (callingTypeName.EndsWith ("."))
-					callingTypeName = callingTypeName.Substring (0, callingTypeName.Length - 1);
-				callingType = Type.GetType (callingTypeName) ?? FindType (callingTypeName);
-				if (callingType != null)
-					break;
-
-				int ix = callingTypeName.LastIndexOf ('.');
-				if (ix == -1)
-					break;
-
-				callingTypeName = callingTypeName.Substring (0, ix);
-				}
-			
 			return callingType == null ? null : callingType.Assembly;
-			}
-
-		private static void GetAllAssemblies ()
-			{
-			var path = InitialParametersClass.ProgramDirectory.ToString ();
-			var dlls = Directory.GetFiles (path, "*.dll").Concat (Directory.GetFiles (path, "*.exe"));
-			foreach (var dll in dlls)
-				{
-				Assembly assembly;
-				try
-					{
-					assembly = Assembly.LoadFrom (dll);
-					}
-				catch
-					{
-					continue;
-					}
-
-				if (assembly == null)
-					continue;
-
-				assemblies.Add (assembly);
-				}
-			}
-
-		private static Type FindType (string typeName)
-			{
-			return assemblies.Select (assembly => assembly.GetType (typeName)).FirstOrDefault (type => type != null);
 			}
 		}
 	}
---
+++ SSharpReflectionExtensionsLibrary/MethodBaseEx.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Crestron.SimplSharp;
+using Crestron.SimplSharp.CrestronIO;
+
+namespace Crestron.SimplSharp.Reflection
+	{
+	public static class MethodBaseEx
+		{
+		public static MethodBase GetCurrentMethod ()
+			{
+			return GetStackMethod (0);
+			}
+
+		public static MethodBase GetStackMethod (int level)
+			{
+			if (level < 0)
+				throw new ArgumentOutOfRangeException ("level");
+
+			var caller = ReflectionUtilities.GetCaller (level + 2);
+			var callingType = ReflectionUtilities.GetCallingType (caller);
+			if (callingType == null)
+				return null;
+
+			var ix = caller.IndexOf ('(');
+			string fullMethodName = caller.Substring (0, ix);
+			string methodName = fullMethodName.Substring (fullMethodName.LastIndexOf ('.') + 1);
+			if (methodName[methodName.Length - 1] == ']')
+				methodName = methodName.Substring (0, methodName.IndexOf ('['));
+			string parString = caller.Substring (ix + 1, caller.Length - ix - 2);
+
+			CType[] types;
+			if (parString.Length != 0)
+				{
+				string[] pars = parString.Split (',');
+				types = pars.Select (p => (CType)ReflectionUtilities.FindType (p.Split (' ')[0])).ToArray ();
+				}
+			else
+				types = CTypeExtensions.CTypeEmptyArray;
+
+			if (types.Any (t => t == null))
+				{
+				var members = callingType.GetMember (methodName);
+				var methods = members.Where (m => m.MemberType == MemberTypes.Method).ToArray ();
+
+				if (methods.Length != 1)
+					return null;
+
+				return (MethodBase)methods[1];
+				}
+
+			return callingType.GetMethod (methodName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static, null, types, null);
+			}
+
+		public static string GetCurrentMethodNameWithParameters ()
+			{
+			return GetStackMethodNameWithParameters (0);
+			}
+
+		public static string GetStackMethodNameWithParameters (int level)
+			{
+			return ReflectionUtilities.GetCaller (level + 2);
+			}
+
+		public static string GetCurrentMethodName ()
+			{
+			return GetStackMethodName (0);
+			}
+
+		public static string GetStackMethodName (int level)
+			{
+			var caller = ReflectionUtilities.GetCaller (level + 2);
+			var ix = caller.IndexOf ('(');
+			return caller.Substring (0, ix);
+			}
+
+		public static MethodBase GetCallingMethod ()
+			{
+			return GetStackMethod (1);         
+			}
+
+		public static string GetCallingMethodNameWithParameters ()
+			{
+			return GetStackMethodNameWithParameters (1);
+			}
+
+		public static string GetCallingMethodName ()
+			{
+			return GetStackMethodName (1);
+			}
+		}
+	}
