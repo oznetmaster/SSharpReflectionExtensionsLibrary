930f3d151a26f73feee9164284e351c523ab2d73
oznetmaster <ncolvin@nivloc.com>
2017-03-09 16:58:46 +0800

    Update copyright and license info

--- SSharpReflectionExtensionsLibrary/Properties/AssemblyInfo.cs
+++ SSharpReflectionExtensionsLibrary/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle ("SSharpReflectionExtensionsLibrary")]
-[assembly: AssemblyCompany ("")]
+[assembly: AssemblyCompany ("Nivloc Enterprises Ltd")]
 [assembly: AssemblyProduct ("SSharpReflectionExtensionsLibrary")]
-[assembly: AssemblyCopyright ("Copyright ©  2014")]
+[assembly: AssemblyCopyright ("Copyright © 2017 by Nivloc Enterprises Ltd")]
 [assembly: AssemblyVersion ("1.0.0.*")]
 
