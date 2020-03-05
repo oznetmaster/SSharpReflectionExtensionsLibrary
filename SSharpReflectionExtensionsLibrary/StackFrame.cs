7eb75634ef3fdd2f2496ecaafb77df6f02853b00
oznetmaster <ncolvin@marvelous.com>
2020-03-05 18:37:49 +0800

    Add the StackFrame and StackTrace classes
    Add the StackFrame and StackTrace classes to the SSharp.Diagnostics namespace

---
+++ SSharpReflectionExtensionsLibrary/StackFrame.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Crestron.SimplSharp;
+using System.Runtime.InteropServices;
+using Crestron.SimplSharp.Reflection;
+
+namespace SSharp.Diagnostics
+	{
+	[SerializableAttribute]
+	[ComVisibleAttribute (true)]
+	public class StackFrame
+		{
+		private MethodBase _methodBase;
+
+		public StackFrame ()
+			{
+			Frame = ReflectionUtilities.GetStackFrame (0);
+			}
+
+		public StackFrame (int skipFrames)
+			{
+			Frame = ReflectionUtilities.GetStackFrame (skipFrames);
+			}
+
+		internal StackFrame (string stackFrame)
+			{
+			Frame = stackFrame;
+			}
+
+		public MethodBase GetMethod ()
+			{
+			return _methodBase ?? (_methodBase = MethodBaseEx.GetMethodFromStackFrame (Frame));
+			}
+
+		internal string Frame { get; private set; }
+		}
+	}
